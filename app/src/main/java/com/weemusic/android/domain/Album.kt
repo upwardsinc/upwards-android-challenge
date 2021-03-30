6f3c80de038ae9cfd27f10effac337976f750fb8
Alex L <alexlivenson@gmail.com>
2021-03-30 12:36:11 -0700

    Alex L - some clean up

--- app/src/main/java/com/weemusic/android/domain/Album.kt
+++ app/src/main/java/com/weemusic/android/domain/Album.kt
@@ -5,10 +5,7 @@ import org.threeten.bp.LocalDate
 data class Album(
     val id: Int,
     val name: String,
-    val images: List<String>,
-    val rights: String,
     val title: String,
     val artist: String,
-    val category: String,
     val releaseDate: LocalDate
 )
